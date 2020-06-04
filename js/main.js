fb672ccc18dc5450b18f5565055fc691961c523d
Patrik Michalak <patrik.michalak@continuent.com>
2020-06-04 17:11:56 +0200

    Sale game canvas, remove mobile config

--- js/main.js
+++ js/main.js
@@ -1,20 +1,10 @@
-let isMobile = navigator.userAgent.indexOf("Mobile");
-
-if (isMobile == -1) {
-    isMobile = navigator.userAgent.indexOf("Tablet");
-}
-
-const config = {
+const game = new Phaser.Game({
     type: Phaser.AUTO,
     width: 960, // window.innerWidth - 100,
     height: 640, // window.innerHeight - 100,
     parent: 'app-canvas',
     scene: [SceneMain],
-};
-
-if (isMobile != -1) {
-    config.width = 480;
-    config.height = 640;
-}
-
-const game = new Phaser.Game(config);
+    scale: {
+        zoom: 2
+    }
+});
