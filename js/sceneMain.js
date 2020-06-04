4a18312cd9bd2275b330c9293110631db8b912c1
Patrik Michalak <patrik.michalak@continuent.com>
2020-06-04 18:01:08 +0200

    Improve trains and semaphores rendering, handle train and semaphore collision

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -1,3 +1,58 @@
+const semState = {
+    STOP: 'STOP',
+    GO: 'GO'
+};
+
+const semPosition = {
+    ABOVE: 'ABOVE',
+    RIGHT: 'RIGHT'
+}
+
+const semaphores = [
+    {
+        x: 13,
+        y: 2,
+        state: semState.STOP,
+        position: semPosition.ABOVE
+    },
+    {
+        x: 11,
+        y: 4,
+        state: semState.STOP,
+        position: semPosition.RIGHT
+    }
+];
+
+const trains = [
+    {
+        trainSelector: 'train1',
+        startPosition: { x: 17, y: 8 },
+        pathPositions: [
+            { x: 17, y: 2 },
+            { x: 2, y: 2 },
+            { x: 2, y: 5 },
+            { x: 5, y: 5 },
+            { x: 5, y: 2 },
+            { x: 17, y: 2 },
+            { x: 17, y: 8 },
+        ],
+    },
+    {
+        trainSelector: 'train2',
+        startPosition: { x: 11, y: 8 },
+        pathPositions: [
+            { x: 11, y: 2 },
+            { x: 2, y: 2 },
+            { x: 2, y: 5 },
+            { x: 5, y: 5 },
+            { x: 5, y: 2 },
+            { x: 11, y: 2 },
+            { x: 11, y: 8 },
+        ],
+    }
+];
+
+
 class SceneMain extends Phaser.Scene {
     constructor() {
         super('SceneMain');
@@ -9,7 +64,10 @@ class SceneMain extends Phaser.Scene {
 
         this.load.image('train', "assets/train.png");
 
-        // this.load.multiatlas('rails', 'assets/rails.json', 'assets');
+        this.load.image('semRed', 'assets/semaphores/red.png');
+        this.load.image('semGreen', 'assets/semaphores/green.png');
+
+        // TODO render semaphore with animation
         // for (const sem in semType) {
         //     this.load.spritesheet(sem, "assets/semaphores/" + sem.toLowerCase() + ".png", { frameWidth: 100, frameHeight: 300 });
         // }
@@ -33,26 +91,30 @@ class SceneMain extends Phaser.Scene {
         map.createStaticLayer('Rails', tileset);
 
         this.cameras.main.setBackgroundColor('#008100');
+
+        // show grid (dev/debug)
         this.grid = new AlignGrid(gridConfig);
         this.grid.showNumbers();
 
-        // // render railway (old way)
-        // rails.forEach(rail => {
-        //     const rimg = this.add.image(0, 0, 'rails', rail.type).setInteractive();
-        //     this.grid.placeAt(rail.x, rail.y, rimg);
+        // render semaphores
+        semaphores.forEach(sem => {
+            const semPositionX = sem.position === semPosition.RIGHT ? sem.x + 1 : sem.x;
+            const semPositionY = sem.position === semPosition.ABOVE ? sem.y - 1 : sem.y;
+            const semStateColor = sem.state === semState.GO ? 'semGreen' : 'semRed';
 
-        //     rimg.displayWidth = game.config.width / gridConfig.cols;
-        //     rimg.scaleY = rimg.scaleX;
+            const semImg = this.add.sprite(this.gridToPx(semPositionX), this.gridToPx(semPositionY), semStateColor).setInteractive();
+            semImg.displayWidth = game.config.width / gridConfig.cols;
+            semImg.scaleY = semImg.scaleX;
 
-        //     rail.isSelected = false;
+            semImg.on('pointerdown', () => {
+                sem.state = sem.state === semState.GO ? semState.STOP : semState.GO;
+                const newSemStateColor = sem.state === semState.GO ? 'semGreen' : 'semRed';
+                semImg.setTexture(newSemStateColor);
+            });
+        });
 
-        //     rimg.on('pointerdown', () => {
-        //         rail.isSelected ? rimg.clearTint() : rimg.setTint(0xaaaaaa);
-        //         rail.isSelected = !rail.isSelected;
-        //     });
 
-        //     // render semaphore
-        //     if (rail.sem) {
+        // TODO render semaphore with animation
         //         const animName = rail.sem.type + 'light';
 
         //         this.anims.create({
@@ -71,88 +133,63 @@ class SceneMain extends Phaser.Scene {
 
         //         semimg.displayHeight = game.config.height / gridConfig.rows;
         //         semimg.scaleX = semimg.scaleY;
-        //     }
-        // });
 
-        
-        // train 1
-        const path1 = new Phaser.Curves.Path(this.gridToPx(17), this.gridToPx(8));
-        path1.lineTo(this.gridToPx(17), this.gridToPx(2));
-        path1.lineTo(this.gridToPx(2), this.gridToPx(2));
-        path1.lineTo(this.gridToPx(2), this.gridToPx(5));
-        path1.lineTo(this.gridToPx(5), this.gridToPx(5));
-        path1.lineTo(this.gridToPx(5), this.gridToPx(2));
-        path1.lineTo(this.gridToPx(17), this.gridToPx(2));
-        path1.lineTo(this.gridToPx(17), this.gridToPx(8));
 
-        this.train1 = this.add.follower(path1, this.gridToPx(17), this.gridToPx(8), 'train');
-        this.train1.displayHeight = game.config.height / gridConfig.rows;
-        this.train1.scaleX = this.train1.scaleY;
-
-        this.train1.startFollow({
-            duration: 5000,
-            yoyo: true,
-            repeat: -1,
-            rotateToPath: true,
-            verticalAdjust: true
-        });
-
-        // train 2
-        const path2 = new Phaser.Curves.Path(this.gridToPx(11), this.gridToPx(8));
-        path2.lineTo(this.gridToPx(11), this.gridToPx(2));
-        path2.lineTo(this.gridToPx(2), this.gridToPx(2));
-        path2.lineTo(this.gridToPx(2), this.gridToPx(5));
-        path2.lineTo(this.gridToPx(5), this.gridToPx(5));
-        path2.lineTo(this.gridToPx(5), this.gridToPx(2));
-        path2.lineTo(this.gridToPx(11), this.gridToPx(2));
-        path2.lineTo(this.gridToPx(11), this.gridToPx(8));
-
-        this.train2 = this.add.follower(path2, this.gridToPx(11), this.gridToPx(8), 'train');
-        this.train2.displayHeight = game.config.height / gridConfig.rows;
-        this.train2.scaleX = this.train2.scaleY;
-
-        this.train2.startFollow({
-            duration: 5000,
-            yoyo: true,
-            repeat: -1,
-            rotateToPath: true,
-            verticalAdjust: true
-        });
-
-        // display paths
+        // display paths (dev/debug)
         const graphics = this.add.graphics();
         graphics.lineStyle(1, 0xFFFF00, 1);
-        path1.draw(graphics, 128);
-        path2.draw(graphics, 128);
+
+        // render trains and their paths
+        trains.forEach(train => {
+            const trainSelector = train.trainSelector;
+            const path = new Phaser.Curves.Path(this.gridToPx(train.startPosition.x), this.gridToPx(train.startPosition.y));
+
+            train.pathPositions.forEach(position => {
+                path.lineTo(this.gridToPx(position.x), this.gridToPx(position.y));
+            });
+
+            this[trainSelector] = this.add.follower(path, this.gridToPx(train.startPosition.x), this.gridToPx(train.startPosition.y), 'train');
+            this[trainSelector].displayHeight = game.config.height / gridConfig.rows;
+            this[trainSelector].scaleX = this[trainSelector].scaleY;
+
+            this[trainSelector].startFollow({
+                duration: 5000,
+                yoyo: true,
+                repeat: -1,
+                rotateToPath: true,
+                verticalAdjust: true
+            });
+
+            path.draw(graphics, 128);
+        });
     }
 
     update() {
-        //     const trainPosition = this.grid.getCellCoord(this.train.x, this.train.y);
+        // check whether trains collide with semaphores
+        trains.forEach(train => {
+            const trainSelector = train.trainSelector;
+            const trainPosition = this.grid.getCellCoord(this[trainSelector].x, this[trainSelector].y);
 
-        //     const currentRail = rails.find(rail => {
-        //         if (rail.x === trainPosition.x && rail.y === trainPosition.y) {
-        //             return true;
-        //         }
-        //     });
+            const trainOnSemaphore = semaphores.find(sem => {
+                if (sem.x === trainPosition.x && sem.y === trainPosition.y) {
+                    return true;
+                }
+            });
 
-        //     // if train is on rail and rail is selected, continue movement
-        //     if (currentRail && currentRail.type && currentRail.isSelected) {
-        //         switch (currentRail.type) {
-        //             case (railType.HORIZONTAL):
-        //                 this.train.x += 2;
-        //                 break;
-        //             case (railType.VERTICAL): {
-        //                 this.train.y += 2;
-        //                 break;
-        //             }
-        //             case (railType.LB): {
-        //                 this.train.x += 1;
-        //                 this.train.y += 1;
-        //                 break;
-        //             }
-        //             default:
-        //                 break;
-        //         }
-        //     }
+            // if train is on rail where semaphore is placed
+            if (trainOnSemaphore) {
+                switch (trainOnSemaphore.state) {
+                    case (semState.STOP):
+                        this[trainSelector].pauseFollow();
+                        break;
+                    case (semState.GO): {
+                        this[trainSelector].resumeFollow();
+                        break;
+                    }
+                    default:
+                        break;
+                }
+            }
+        });
     }
 }
