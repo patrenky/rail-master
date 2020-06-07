61400e9595089dab293b6eb75f1b1509cd338a28
Jan Havlík <jan.havlik@firma.seznam.cz>
2020-06-07 22:13:49 +0200

    replaced semaphores with bullshit

--- assets/semaphores.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.1" name="semaphores" tilewidth="16" tileheight="16" tilecount="20" columns="4">
- <image source="semaphores.png" width="64" height="80"/>
-</tileset>
